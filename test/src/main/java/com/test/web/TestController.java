91069be9d5497b82f88535cfe9ae33441fbda48a
ALazyDogXD <wangzhihao1114@gmail.com>
2022-05-28 20:21:25 +0800

    关联逻辑维护以及部分测试代码

--- test/src/main/java/com/test/web/TestController.java
+++ test/src/main/java/com/test/web/TestController.java
@@ -1,24 +1,13 @@
 package com.test.web;
 
 import com.test.entity.A;
-import com.test.entity.B;
 import com.test.service.AService;
 import com.test.service.BService;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.util.Units;
-import org.apache.poi.xwpf.usermodel.*;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblWidth;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 import javax.annotation.Resource;
-import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 /**
